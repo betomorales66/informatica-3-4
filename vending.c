e0685aa2b348770e6a678b6bedeb67e52b177b53
betomorales66 <beto001ms@gmail.com>
2026-04-21 15:48:19 +0000

    commit

---
+++ vending.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(voi){
+    printf("Vending Machine Stock\n");
+    printf("-----------------------\n");
+    //tortas chumas
+    float price1 = 74.99;
+    int quantity1 = 15;
+    char select1 = 't';
+
+    //mango ice cream
+    float price2 = 30.50;
+    int quantity2 = 20;
+    char select2 = 'm';
+
+    //pretzels
+    float price3 = 15.00;
+    int quantity3 = 20;
+    char select3 = 'p';
+
+    //print for vending machine
+    printf("Vending Machine Stock\n");
+    printf("-----------------------\n");
+    printf("item name: Tortas Chumas\n");
+
+
+
+
+}
