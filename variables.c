827d3077a6b9390b16fcd8f1de7d2f2eef097901
betomorales66 <beto001ms@gmail.com>
2026-04-21 14:58:25 +0000

    commit

---
+++ variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// put the include
+int main(void){
+int number;
+// remove symbols not allowed
+int test_value;
+//remove the space between those words
+int example;
+//remove the number
+float int_count;
+//add the count to the int
+printf("Variables created successfully.\n");
+}
