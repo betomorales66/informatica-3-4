d12451c7b9df941ef4142a6677eaca33d2230506
betomorales66 <beto001ms@gmail.com>
2026-04-23 21:09:30 +0000

    hello2

--- hello.c
+++ hello.c
@@ -2,5 +2,20 @@
 
 int main(void)
 {
-    printf("Hello Beto! \n");
+    char name[50];
+    printf("Enter your name: ");
+    scanf("%s", &name);
+    printf("Hello %s!\n", name);
+
+    char color[50];
+    printf("Enter your favorite color: ");
+    scanf("%s", &color);
+    printf("your favorite color is %s!\n", color);
+
+    int age[50];
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    printf("your age is %d!\n", age);
+
+
 }
